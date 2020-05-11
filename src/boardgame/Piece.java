1ece595518f6adc7a6bdbb0f01ba83d2310d31b9
julianmf <figueredo.juliano@hotmail.com>
2020-05-10 21:10:15 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -12,5 +12,26 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	//hook method
+	//https://stackoverflow.com/questions/4797465/difference-between-hooks-and-abstract-methods-in-java
+	// design patterns - http://ima.udg.edu/~sellares/EINF-ES1/TemplateMethodToni.pdf
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean [][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
