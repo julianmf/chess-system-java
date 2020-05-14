06b985de633dffd5d605bd6286ba623a2f7a0847
julianmf <figueredo.juliano@hotmail.com>
2020-05-14 00:11:37 -0300

    Check logic and regression

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,11 +17,11 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
+
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch,captured);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -29,17 +29,15 @@ public class Program {
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-				if(captured != null) {
+
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
